e0173cd23e37b8028b3a38d75dfff3ed026b6a14
Brett McCaffray <Brett@bmp.local>
2014-10-30 23:10:57 -0700

    add prompts for package init

--- app/index.js
+++ app/index.js
@@ -22,14 +22,35 @@ var AppgGenerator = yeoman.generators.Base.extend({
         var done = this.async();
 
         var prompts = [{
-            type: 'checkbox',
-            name: 'init',
-            message: 'Project Name:',
-            choices: [{
-                    name: 'Express Server',
-                    value: 'grunt-express-server'
-                }],
+            type: 'input',
+            name: 'initName',
+            message: 'Name:',
             default: ''
+        }, {
+            type: 'input',
+            name: 'initVersion',
+            message: 'Version:',
+            default: ''
+        }, {
+            type: 'input',
+            name: 'initDesc',
+            message: 'Description:',
+            default: ''
+        }, {
+            type: 'input',
+            name: 'initAuthor',
+            message: 'Author:',
+            default: ''
+        }, {
+            type: 'input',
+            name: 'initKeys',
+            message: 'Keywords:',
+            default: ''
+        }, {
+            type: 'input',
+            name: 'initLicense',
+            message: 'License:',
+            default: 'MIT'
         }, {
             type: 'checkbox',
             name: 'adds',
